1a3cb1f763eb1770b4f5a219e7419afd28ad0508
Holayn <kwwong1597@gmail.com>
2018-08-24 14:41:36 -0400

    removed crypto from dependencies

--- test.js
+++ test.js
@@ -1,24 +1,11 @@
 var test = require('./index');
 
-const res = test.encrypt('wendy sux', 'asdf');
-res.then((str) => {
-  test.decrypt(str, 'asdf').then((asdfasdf) => {
+const res = test.encrypt('123', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+res.then((str) => {;
+  test.decrypt(str, 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3').then((asdfasdf) => {
     console.log(asdfasdf);
   })
-
-  }
-}).catch((err) => {
-  console.log(err);
+  test.decrypt("yinMG9s6Xeszx7boI90Ijg==;KYICQOuQZRdQzEDJ5TzM6g==", 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3').then((asdf) => {
+    console.log(asdf);
+  });
 });
-
-
-
-
-class Promise {
- then(cb) {
-
-
-
-  cb(resolveParam)
- }
-}
